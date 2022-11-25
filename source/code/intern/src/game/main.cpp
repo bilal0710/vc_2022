034f2acd70338db5038f003f360d240f46748247
Bilal Alnaani <belal0710@icloud.com>
2022-11-25 11:55:05 +0100

    add some new shapes

--- source/code/intern/src/game/main.cpp
+++ source/code/intern/src/game/main.cpp
@@ -1,22 +1,46 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    // create the window
+    sf::RenderWindow window(sf::VideoMode(1200, 900), "My window");
+    sf::CircleShape shape(200.f);
 
+    // define an octagon
+    sf::CircleShape octagon(80.f, 8);
+
+    shape.setFillColor(sf::Color(150, 50, 25));
+    octagon.setFillColor(sf::Color::Cyan);
+
+    // set a 10-pixel wide orange outline
+    shape.setOutlineThickness(10.f);
+    shape.setOutlineColor(sf::Color(250, 150, 100));
+
+    // set the absolute position of the octagon
+    octagon.setPosition(100.f, 100.f);
+
+    // run the program as long as the window is open
     while (window.isOpen())
     {
+        // check all the window's events that were triggered since the last iteration of the loop
         sf::Event event;
         while (window.pollEvent(event))
         {
+            // "close requested" event: we close the window
             if (event.type == sf::Event::Closed)
                 window.close();
         }
+     
+     
+        // clear the window with selected color -> Background
+        window.clear(sf::Color::Black);
 
-        window.clear();
+        // draw everything here...
         window.draw(shape);
+        window.draw(octagon);
+
+        // end the current frame
         window.display();
     }
 

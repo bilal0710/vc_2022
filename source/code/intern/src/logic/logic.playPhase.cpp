f13e5d218e6ad24c5194c9eb75192bef254d0380
Bilal Alnaani <belal0710@icloud.com>
2023-04-26 22:32:32 +0200

    update logic play phase

--- source/code/intern/src/logic/logic.playPhase.cpp
+++ source/code/intern/src/logic/logic.playPhase.cpp
@@ -24,6 +24,12 @@ namespace Logic
 		//cout << "Logic::PlayPhase::InternOnRun" << std::endl;
 		//cout << "------------------------" << std::endl;
 
+		if (!m_PlayerCollidedWithGround) {
+			//std::cout << "No Ground" << std::endl;
+			MovePlayer(Core::Float2(0.0f, m_Step));
+			return;
+		}
+
 		Logic::CInputSystem& rInputSystem = Logic::CInputSystem::GetInstance();
 		SInputType::Enum currentInput;
 
@@ -34,16 +40,16 @@ namespace Logic
 			switch (currentInput)
 			{
 			case SInputType::Enum::MoveUp:
-				MovePlayer(Core::Float2(0.0f, -2.0f));
+				MovePlayer(Core::Float2(0.0f, -m_Step));
 				break;
 			case SInputType::Enum::MoveDown:
-				MovePlayer(Core::Float2(0.0f, 2.0f));
+				MovePlayer(Core::Float2(0.0f, m_Step));
 				break;
 			case SInputType::Enum::MoveRight:
-				MovePlayer(Core::Float2(2.0f, 0.0f));
+				MovePlayer(Core::Float2(m_Step, 0.0f));
 				break;
 			case SInputType::Enum::MoveLeft:
-				MovePlayer(Core::Float2(-2.0f, 0.0f));
+				MovePlayer(Core::Float2(-m_Step, 0.0f));
 				break;
 			default:
 				break;
@@ -65,49 +71,72 @@ namespace Logic
 		Data::CPlayerSystem& rPlayerSystem = Data::CPlayerSystem::GetInstance();
 		Data::CEntity* pPlayer = rPlayerSystem.GetPlayer();
 		Data::CEventSystem& rEventSystem = Data::CEventSystem::GetInstance();
+
 		if (pPlayer != nullptr)
 		{
+			if (pPlayer->position[1] > 1200)
+			{
+				rEventSystem.FireEvent(2);
+			}
+
+
 			// Check Collisions
-			// TODO: This needs to replaced with the proper EntityMap System call with the new aabb after the move (massive performance increase)
 			Data::CEntitySystem& rEntitySystem = Data::CEntitySystem::GetInstance();
 			auto Entities = rEntitySystem.GetAllEntities();
 
 			std::vector<Data::CEntity*> collisionEntities;
 
-			bool playerCollidedWithFinish = false;
-
 			for (auto& Entity : Entities)
 			{
 				if (Core::CAABB3<float>(
 					Core::Float3(pPlayer->position[0], pPlayer->position[1], pPlayer->position[2]),
-					Core::Float3(pPlayer->position[0] + 64, pPlayer->position[1] + 64, pPlayer->position[2])
+					Core::Float3(pPlayer->position[0] + 74, pPlayer->position[1] + 128, pPlayer->position[2])
 				).Intersects(Entity->aabb))
 				{
+					std::cout << pPlayer->position[1] << std::endl;
 					if (Entity->category == Data::SEntityCategory::Finish)
 					{
 						rEventSystem.FireEvent(2);
 
 					}
-				}
-			}
 
-			if (collisionEntities.empty())
-			{
-				pPlayer->position = Core::Float3(pPlayer->position[0] + _Step[0], pPlayer->position[1] + _Step[1], pPlayer->position[2]);
-				pPlayer->aabb = Core::CAABB3<float>(
-					Core::Float3(pPlayer->position[0], pPlayer->position[1], pPlayer->position[2]),
-					Core::Float3(pPlayer->position[0] + 64, pPlayer->position[1] + 64, pPlayer->position[2])
-				);
-			}
-			else
-			{
-				std::cout << collisionEntities.size() << std::endl;
-			}
+					if (Entity->category == Data::SEntityCategory::Ground)
+					{
+						collisionEntities.push_back(Entity);
+						pPlayer->position = Core::Float3(pPlayer->position[0] + _Step[0], pPlayer->position[1] + _Step[1], pPlayer->position[2]);
+						pPlayer->aabb = Core::CAABB3<float>(
+							Core::Float3(pPlayer->position[0], pPlayer->position[1], pPlayer->position[2]),
+							Core::Float3(pPlayer->position[0] + 64, pPlayer->position[1] + 64, pPlayer->position[2])
+						);
 
+					}
+				
+			}
 		}
+
+		if (collisionEntities.empty())
+		{
+			m_PlayerCollidedWithGround = false;
+			//rEventSystem.FireEvent(2);
+			pPlayer->position = Core::Float3(pPlayer->position[0] + _Step[0], pPlayer->position[1] + _Step[1], pPlayer->position[2]);
+			pPlayer->aabb = Core::CAABB3<float>(
+				Core::Float3(pPlayer->position[0], pPlayer->position[1], pPlayer->position[2]),
+				Core::Float3(pPlayer->position[0] + 64, pPlayer->position[1] + 64, pPlayer->position[2])
+			);
+			
+		}
+		else
+		{
+			//std::cout << collisionEntities.size() << std::endl;
+		}
+
 	}
+}
 
 
-	void CPlayPhase::OnLeave()
-	{}
+void CPlayPhase::OnLeave()
+{
+	m_PlayerCollidedWithGround = true;
+	std::cout << "OnLeave" << std::endl;
+}
 }
